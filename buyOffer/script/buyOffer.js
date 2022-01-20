e28a4fa80ca8192dad28a95408e72d53ab11b7f2
CristianAlegreBustos <cristiannahuelalegrebustos@gmail.com>
2022-01-20 19:18:12 -0300

    update the website

--- buyOffer/script/buyOffer.js
+++ buyOffer/script/buyOffer.js
@@ -11,7 +11,7 @@ var obj;
 
 function getData(){
     var req = new XMLHttpRequest();
-    req.open('GET', 'https://cristianalegrebustos.github.io/ShoppingCart/buyOffer/script/db.json', true); 
+    req.open('GET', 'https://cristianalegrebustos.github.io/ShoppingCart/db.json', true); 
     req.onreadystatechange = function () {
         if (req.readyState == 4 && req.status == 200) {
             obj = JSON.parse(req.responseText);
